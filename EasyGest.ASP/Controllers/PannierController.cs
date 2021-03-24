5c472b17dd6f80fddce870dbf4174e4def15658e
bryan dane kwuimo kwekam <kwuimobryan@hotmail.com>
2021-03-24 09:31:58 +0100

    Ajout du porjet ASP

---
+++ EasyGest.ASP/Controllers/PannierController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyGest.ASP.Controllers
+{
+    public class PannierController : Controller
+    {
+        // GET: Pannier
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EasyGest.ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EasyGest.ASP.MvcApplication" Language="C#" %>
